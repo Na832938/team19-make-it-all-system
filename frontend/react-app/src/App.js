5b0b5c6a64aa0785f4d53c2ff83e5b0ddbd8d849
Nanda <N.Kumudhan1-24@student.lboro.ac.uk>
2025-10-23 16:24:50 +0100

    Completely restructured the app on the react branch: - Split every piece of functionality into smaller, focused components - Made everything modular and reusable - Converted to a proper JS React app - Separate CSS files for easy styling
    Way more maintainable now!

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,7 +1,20 @@
-// src/App.jsx
-import React from 'react';
-import EmployeeDashboard from './EmployeeDashboard.jsx';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TodoPage from "./TodoPage";
+import PostPage from "./PostPage";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
 
-export default function App() {
-  return <EmployeeDashboard />;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/todo" element={<TodoPage />} />
+        <Route path="/posts" element={<PostPage />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
