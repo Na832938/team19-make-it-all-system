b130c54f5ade357da2fbbc7017a46005ee607db3
Nanda Kumudhan <108399547+nanda-kumudhan@users.noreply.github.com>
2025-11-18 22:40:39 +0000

    fixed vite config for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/team19-make-it-all-system/', // ✔ correct for GitHub Pages
+  base: '/team19-make-it-all-system/', // correct GitHub Pages base path
   build: {
     outDir: 'dist',
   },
